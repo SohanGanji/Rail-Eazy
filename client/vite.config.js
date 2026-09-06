60abc941ae0078bcc66f6820a898524d59735e3d
Sohan Ganji <sohan.ganji2151@gmail.com>
2026-09-06 22:51:31 +0530

    feat(client): add vercel.json rewrites config and update vite port to 5173

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
-      '/health': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+    },
+  },
+});
